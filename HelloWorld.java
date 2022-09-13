72d15d237cf18d7f73c95cac012c71992e6dd243
jmstechhome19 <112363179+jmstechhome19@users.noreply.github.com>
2022-09-13 08:06:31 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -35,6 +35,8 @@
 public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hello, World..........DevOps Guys .................");
+    System.out.println("Hello, World..........DevOps Guys .................");
+    System.out.println("Hello, World..........DevOps Guys .................");
 
   }
 }
