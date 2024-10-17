ddfe12b54b3ddd98abb59bc08e04ef0c467e422e
sksitharam <sksitharam@gmail.com>
2024-10-17 17:33:30 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -39,10 +39,5 @@ public class HelloWorld {
     System.out.println("Hello, World..........DevOps Guys .................");
         System.out.println("Hello, World..........DevOps Guys .................");
     System.out.println("Hello, World..........DevOps Guys .................");
-    System.out.println("Hello, World..........DevOps Guys .................");
-        System.out.println("Hello, World..........DevOps Guys .................");
-    System.out.println("Hello, World..........DevOps Guys .................");
-    System.out.println("Hello, World..........DevOps Guys .................");
-
   }
 }
